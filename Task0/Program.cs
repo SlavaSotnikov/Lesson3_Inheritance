e0c638556d36410c4399e93a5ef7f08c1e0dc4bf
Slava Sotnikov <SlavaSotnikov2020@gmail.com>
2020-11-08 21:57:38 +0200

    Add project files.

---
+++ Task0/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task0
+{
+    class Program
+    {
+        static void Main()
+        {            
+            RedPrinter red = new RedPrinter();
+            GreenPrinter green = new GreenPrinter();
+            YellowPrinter yellow = new YellowPrinter();
+
+            Printer[] print = new Printer[3];
+            print[0] = red;
+            print[1] = green;
+            print[2] = yellow;
+
+            for (int i = 0; i < print.Length; i++)
+            {
+                print[i].Print("Inheritance & Polymorphism");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task0/YellowPrinter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task0
+{
+    class YellowPrinter:Printer
+    {
+        public override void Print(string value)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"{value}.");
+        }
+    }
+}
